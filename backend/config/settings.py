9f5495c916d1102d54f749e9ee83bc0e89911509
KarimI-I <kareemsafarofficial@gmail.com>
2023-08-05 15:51:25 +0300

    frontend mapping done

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "frontend/project/build")],
+        "DIRS": [os.path.join(BASE_DIR, 'client/build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -144,9 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend/project/build/static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'client/build/static')
+]
